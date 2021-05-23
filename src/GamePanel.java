b665731fd9770ab187ef12fdb2fa20f4d1521e74
marcelo-maeda-jr <marcelo.yoshi.maeda@gmail.com>
2021-05-23 13:49:30 -0300

    newApple method setup and add newApple to draw method

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -54,10 +54,13 @@ public class GamePanel extends JPanel implements ActionListener {
 			g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
 			g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
 		}
+		g.setColor(Color.red);
+		g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
 	}
 	
 	public void newApple() {
-		
+		appleX = random.nextInt((int)SCREEN_WIDTH/UNIT_SIZE) * UNIT_SIZE;
+		appleY = random.nextInt((int)SCREEN_HEIGHT/UNIT_SIZE) * UNIT_SIZE;
 	}
 	
 	public void move() {
