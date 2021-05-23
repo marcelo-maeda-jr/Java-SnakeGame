024b7d5ce410e30260811ff633d7f7a52c278e87
marcelo-maeda-jr <marcelo.yoshi.maeda@gmail.com>
2021-05-23 13:17:07 -0300

    GamePanel constructor setup

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -27,7 +29,12 @@ public class GamePanel extends JPanel implements ActionListener {
 	Random random;
 	
 	GamePanel(){
-		
+		random = new Random();
+		this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT);
+		this.setBackground(Color.BLACK);
+		this.setFocusable(true);
+		this.addKeyListener(new MyKeyAdapter());
+		startGame();
 	}
 	
 	public void startGame() {
