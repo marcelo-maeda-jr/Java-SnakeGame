9997b00fff5d84d6ded8e1bc3d8611ac1cf900ca
marcelo-maeda-jr <marcelo.yoshi.maeda@gmail.com>
2021-05-23 13:39:52 -0300

    paintComponent and draw methos setup

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -45,11 +45,15 @@ public class GamePanel extends JPanel implements ActionListener {
 	}
 	
 	public void paintComponent(Graphics g) {
-		
+		super.paintComponent(g);
+		draw(g);
 	}
 	
 	public void draw(Graphics g) {
-		
+		for(int i=0; i< SCREEN_HEIGHT/UNIT_SIZE; i++) {
+			g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+			g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
+		}
 	}
 	
 	public void newApple() {
